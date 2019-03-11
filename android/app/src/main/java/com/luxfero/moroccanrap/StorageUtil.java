80ba0718639d25742e02667d06052124aedc0576
HARIR Imam (EXT) <iharir@largus.ma>
2019-03-11 10:48:55 +0100

    authorize plain text url

---
+++ android/app/src/main/java/com/luxfero/moroccanrap/StorageUtil.java
@@ -0,0 +1,60 @@
+package com.luxfero.moroccanrap;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+class StorageUtil {
+
+    private final String STORAGE = "com.example.luxfero.mymediaplayer.STORAGE";
+    private SharedPreferences preferences;
+    private Context context;
+
+    StorageUtil(Context context) {
+        this.context = context;
+    }
+
+    void storeAudio(ArrayList<Audio> arrayList) {
+        preferences = context.getSharedPreferences(STORAGE, Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = preferences.edit();
+        Gson gson = new Gson();
+
+        String json = gson.toJson(arrayList);
+        editor.putString("audioArrayList", json);
+        editor.apply();
+    }
+
+    ArrayList<Audio> loadAudio() {
+        preferences = context.getSharedPreferences(STORAGE, Context.MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = preferences.getString("audioArrayList", null);
+        Type type = new TypeToken<ArrayList<Audio>>() {
+        }.getType();
+        return gson.fromJson(json, type);
+    }
+
+    void storeAudioIndex(int index) {
+        preferences = context.getSharedPreferences(STORAGE, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt("audioIndex", index);
+        editor.apply();
+    }
+
+    int loadAudioIndex() {
+        preferences = context.getSharedPreferences(STORAGE, Context.MODE_PRIVATE);
+        return preferences.getInt("audioIndex", -1);//return -1 if no data found
+    }
+
+    void clearCachedAudioPlaylist() {
+        preferences = context.getSharedPreferences(STORAGE, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.clear();
+        editor.apply();
+    }
+}
--- lib/Helpers/globals.dart
+++ lib/Helpers/globals.dart
@@ -5,10 +5,10 @@ class Globals {
     return _singleton;
   }
 
-  String tracksRoot = 'http://206.189.15.19/api/tracks';
-  String artistsRoot = 'http://206.189.15.19/api/artists';
-  String serverPath = 'http://206.189.15.19/';
-  String trackStreamPath = 'http://206.189.15.19/stream/';
+  String tracksRoot = 'http://www.imamharir.com/api/tracks';
+  String artistsRoot = 'http://www.imamharir.com/api/artists';
+  String serverPath = 'http://www.imamharir.com/';
+  String trackStreamPath = 'http://www.imamharir.com/stream/';
   String appKey = 'oF52iOAQNd63Lg9GP5zQkmntmwB1Nf6m';
 
   Globals._internal();
